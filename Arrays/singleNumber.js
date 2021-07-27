d428928f612d0213f894da64eb1ed88f6c030868
KennyHoang-CS <kennyhoangcs@gmail.com>
2021-07-26 19:16:19 -0700

    adding single number array exericse

---
+++ Arrays/singleNumber.js
@@ -0,0 +1,22 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+const singleNumber = function(nums) {
+    
+    let obj = {}; 
+    
+    for (let i = 0; i < nums.length; i++) {
+        if (obj[nums[i]]) {
+            obj[nums[i]] += 1; 
+        } else {
+            obj[nums[i]] = 1; 
+        }
+    }
+    
+    for (key in obj) {
+        if (obj[key] === 1) {
+            return key;
+        }
+    }
+};
