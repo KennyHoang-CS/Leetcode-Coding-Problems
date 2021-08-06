2f3cf946789aa5416d2e992264e24482bedb70e6
KennyHoang-CS <kennyhoangcs@gmail.com>
2021-08-06 00:18:45 -0700

    adding merge tree exercise

--- Math/nthRoot.js
+++ Math/nthRoot.js
@@ -1,10 +1,2 @@
 
-function nthRoot(x, n) {
-    if(x > 0) {
-        return Math.sqrt(x, n);
-        }else {
-        return 1 / Math.sqrt(x, n);
-        }
-}
-
-console.log(nthRoot(7, 3));
+console.log(529 - (26*16))
