6e2daace336e5c4939dc3780a55e74f11a71babe
KennyHoang-CS <kennyhoangcs@gmail.com>
2021-08-08 00:51:19 -0700

    adding smallerNumberThenCurrent array exercise

---
+++ Arrays/smallerNumbersThenCurrent.js
@@ -0,0 +1,22 @@
+/**
+ * @param {number[]} nums
+ * @return {number[]}
+ */
+ var smallerNumbersThanCurrent = function(nums) {
+        
+    let result = [];
+    let count = 0; 
+    for (let i = 0; i < nums.length; i++) {
+        for (let j = 0; j < nums.length; j++) {
+            if (j === i) continue;
+            if (nums[i] > nums[j]) {
+                ++count; 
+            }
+        }
+        result.push(count);
+        count = 0; 
+    }
+    
+    return result; 
+};
+
