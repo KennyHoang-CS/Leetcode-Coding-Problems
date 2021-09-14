e06e0a078162259719676fdd2a75f24b2ea5bd00
KennyHoang-CS <kennyhoangcs@gmail.com>
2021-09-14 15:35:29 -0700

    another bits exercise

---
+++ Bits Operations/hammingWeight.js
@@ -0,0 +1,16 @@
+/**
+ * @param {number} n - a positive integer
+ * @return {number}
+ */
+ var hammingWeight = function(n) {
+    
+    let count = 0;
+    
+    while (n != 0) {
+        count += n & 1; 
+        n >>>= 1; 
+        console.log('n: ', n)
+    }
+    
+    return count;
+};
