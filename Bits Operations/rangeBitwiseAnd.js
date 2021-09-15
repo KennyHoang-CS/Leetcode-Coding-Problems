cedbf106c0137a6aa0d164735e717ccecc7c317f
KennyHoang-CS <kennyhoangcs@gmail.com>
2021-09-15 00:05:51 -0700

    another bits exercise

---
+++ Bits Operations/rangeBitwiseAnd.js
@@ -0,0 +1,16 @@
+/**
+ * @param {number} left
+ * @param {number} right
+ * @return {number}
+ */
+// https://leetcode.com/problems/bitwise-and-of-numbers-range/
+var rangeBitwiseAnd = function(left, right) {
+    let count = 0;
+    while (left !== right) {
+        left >>= 1;
+        right >>= 1;
+        ++count; 
+    }
+    
+    return left << count; 
+};
